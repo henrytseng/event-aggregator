e16818820e010d69802fe4e15d8783fc82a22b45
Henry Tseng <henry@heycanvas.com>
2013-02-27 16:56:30 -0500

    initial code

--- LICENSE.md
+++ LICENSE.md
@@ -1,30 +1,3 @@
-EventAggregator
-================
-
-#### Brief
-
-A nodejs module that emits events after a specified set of events and callbacks have been triggered.  
-
-##### Description
-
-EventAggregator is designed to coodinate events and callbacks dispatched in an asynchonous environment.  
-
-## Installation
-
-	$ npm install event-aggregator
-    
-## Quick Start
-
-
-
-## Examples
-
-
-
-## License
-
-(The MIT License)
-
 Copyright (c) 2013 Henry Tseng (http://www.henrytseng.com)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
---
+++ test/system-event-aggregator.js
@@ -0,0 +1,87 @@
+var Settings = require('../config/settings');
+var should = require('should');
+var Settings = require('../config/settings');
+var EventEmitter = require('events').EventEmitter;
+var EventAggregator = require(Settings.path.system+'/event/aggregator');
+
+describe('EventAggregator', function() {
+	
+	describe('#wait()', function() {
+		it('should add an emitter and multiple events and dispatch trigger', function($done) {
+			var a = new EventEmitter();
+			var aggregator = new EventAggregator();
+			
+			aggregator.waitEvent('complete', a, 'ready');
+			setTimeout(function() {
+				a.emit('ready');
+			}, 1);
+			aggregator.waitEvent('complete', a, 'on');
+			setTimeout(function() {
+				a.emit('on');
+			}, 2);
+			
+			aggregator.on('complete', function() {
+				$done();
+			});
+		});
+		
+		it('should add two callbacks and dispatch trigger', function($done) {
+			var aggregator = new EventAggregator();
+			
+			setTimeout(aggregator.waitCallback('complete'), 1);
+			setTimeout(aggregator.waitCallback('complete'), 1.1);
+			
+			aggregator.on('complete', function() {
+				$done();
+			});
+		});
+		
+		it('should add a callback and an event and dispatch trigger', function($done) {
+			var a = new EventEmitter();
+			var aggregator = new EventAggregator();
+			
+			setTimeout(aggregator.waitCallback('complete'), 1);
+			
+			aggregator.waitEvent('complete', a, 'ready');
+			setTimeout(function() {
+				a.emit('ready');
+			}, 2);
+			
+			aggregator.on('complete', function() {
+				$done();
+			});
+		});
+	});
+	
+	describe('#ignore()', function() {
+		it('should add two callbacks and remove one and dispatch trigger', function($done) {
+			var aggregator = new EventAggregator();
+			var callback = null;
+			
+			setTimeout(aggregator.waitCallback('complete'), 1);
+			callback = aggregator.waitCallback('complete');
+			
+			aggregator.ignoreCallback('complete', callback);
+			aggregator.on('complete', function() {
+				$done();
+			});
+		});
+		
+		it('should add two events and remove one and dispatch trigger', function($done) {
+			var aggregator = new EventAggregator();
+			var a = new EventEmitter();
+			
+			aggregator.waitEvent('complete', a, 'ready');
+			setTimeout(function() {
+				a.emit('ready');
+			}, 1);
+			
+			aggregator.waitEvent('complete', a, 'on');
+			aggregator.ignoreEvent('complete', a, 'on');
+			
+			aggregator.on('complete', function() {
+				$done();
+			});
+		});
+	});
+});
